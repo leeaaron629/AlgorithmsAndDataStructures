d9b261a3a849d02dc197f8547ca4681a7340c145
Aaron Lee <leeaaron326@gmail.com>
2018-11-02 23:30:02 -0400

    Adding PowerSet Algorithm

---
+++ algorithms/PowerSet.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PowerSet {
+	
+	public static void main(String[] args) {
+		PowerSet ps = new PowerSet();
+	}
+	
+	
+    public List<List<Integer>> subsets(int[] nums) {
+        
+    	List<List<Integer>> powerSet = new ArrayList<>();
+    	powerSet.add(new ArrayList<>());
+    	
+    	buildPowerSet(nums, 0, powerSet);
+    	
+    	return powerSet;
+    }
+    
+    public void buildPowerSet(int[] nums, int i, List<List<Integer>> powerSet) {
+    	
+    	if (i >= nums.length) return;
+    	
+    	List<List<Integer>> newSet = new ArrayList<>();
+    	
+    	for (List<Integer> set : powerSet) {
+    		List<Integer> currentSet = new ArrayList<>(set);
+    		currentSet.add(nums[i]);
+    		newSet.add(currentSet);
+    	}
+    	
+    	powerSet.addAll(newSet);
+    	
+    	buildPowerSet(nums, i+1, powerSet);
+    }
+}
