8cb60d8fc2bf675b5e4e359dfae2221772ebc4c9
Aaron Lee <leeaaron326@gmail.com>
2018-11-11 14:20:32 -0500

    Adding TreeNode and ListNode to datastructures package

---
+++ datastructures/TreeNode.java
@@ -0,0 +1,12 @@
+package datastructures;
+
+//Definition for a binary tree node.
+public class TreeNode {
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
