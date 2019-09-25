036c82dcdabb38d4f88bfae1f9fce7dae3b987dc
Aaron Lee <leeaaron326@gmail.com>
2019-09-24 22:02:10 -0400

    adding more questions to do

---
+++ src/FindMinInSortedRotatedArray.java
@@ -0,0 +1,17 @@
+/**
+ * https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+ */
+public class FindMinInSortedRotatedArray {
+
+    public int findMin(int[] nums) {
+
+        // Do a binary search
+
+        // Pretend the array is a sorted array
+
+        // You know the length before hand
+
+        return 0;
+    }
+
+}
---
+++ src/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,10 @@
+/**
+ * https://leetcode.com/problems/longest-repeating-character-replacement/
+ */
+public class LongestRepeatingCharacterReplacement {
+
+    public int characterReplacement(String s, int k) {
+        return 0;
+    }
+
+}
