8750f57eb39c11f0d208af7e31b659f3c43f3e2e
Aaron Lee <leeaaron326@gmail.com>
2019-10-04 08:40:32 -0400

    incomplete copy list with random pointer

--- src/datastructures/Node.java
+++ src/datastructures/Node.java
@@ -4,6 +4,19 @@ public class Node {
 	
 	public Node next;
 	public Node prev;
+	public Node random;
 	public int val;
-	
+
+	public Node() {}
+
+	public Node(int val) {
+		this.val = val;
+	}
+
+	@Override
+	public String toString() {
+		return "Node{" +
+				"val=" + val +
+				'}';
+	}
 }
