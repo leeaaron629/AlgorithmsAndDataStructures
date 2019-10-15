4fb6ae6606a2b9cd0d121b57255ee687f6015d29
Aaron Lee <leeaaron326@gmail.com>
2019-10-15 00:16:09 -0400

    Attempting MatrixV2

--- src/unsolved/Search2DMatrixV2.java
+++ src/unsolved/Search2DMatrixV2.java
@@ -1,10 +1,65 @@
 package unsolved;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * https://leetcode.com/problems/search-a-2d-matrix-ii/
  */
 public class Search2DMatrixV2 {
+
     public boolean searchMatrix(int[][] matrix, int target) {
-        return true;
+
+        if (matrix.length == 0) return false;
+
+        int beg = 0; int end = matrix.length - 1;
+
+        while (beg <= end) {
+            int mid = (beg + end) / 2;
+
+            if (binarySearch(matrix[mid], target)) {
+                return true;
+            } else if (target < matrix[mid][0]) {
+                end = mid - 1;
+            } else {
+                beg = mid + 1;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean binarySearch(int[] arr, int target) {
+
+        int beg = 0; int end = arr.length - 1;
+
+        while (beg <= end) {
+
+            int mid = (beg + end) / 2;
+
+            if (target == arr[mid]) {
+                return true;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                beg = mid + 1;
+            }
+
+        }
+
+        return false;
+    }
+    
+    @Test
+    public void tests() {
+        int[][] matrix = new int[][] {
+                {1,4,7,11,15},
+                {2,5,8,12,19},
+                {3,6,9,16,22},
+                {10,13,14,17,24},
+                {18,21,23,26,30}
+        };
+
+        assert searchMatrix(matrix, 14);
+        assert searchMatrix(matrix, 5);
     }
 }
