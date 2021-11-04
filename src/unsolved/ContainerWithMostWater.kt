9f20ca2ce5822c848fac108f567aa1d0adfa1861
Aaron Lee <leeaaron326@gmail.com>
2021-11-03 23:19:47 -0400

    Added ContainerWithMostWater solution

---
+++ src/unsolved/ContainerWithMostWater.kt
@@ -0,0 +1,37 @@
+package unsolved
+
+/**
+ * https://leetcode.com/problems/container-with-most-water/
+ */
+class ContainerWithMostWater {
+    fun maxArea(height: IntArray): Int {
+
+        var i = 0
+        var j = height.size - 1
+        var maxArea = 0
+
+        while (i < j) {
+
+            val minHeight = height[i].coerceAtMost(height[j])
+            val distance = j - i
+            val curArea = minHeight * distance
+
+            maxArea = maxArea.coerceAtLeast(curArea)
+
+            when {
+                height[i] < height[j] -> i++
+                height[j] < height[i] -> j--
+                else -> i++
+            }
+        }
+
+        return maxArea
+    }
+}
+
+fun main() {
+    val containerWithMostWater = ContainerWithMostWater()
+    containerWithMostWater
+        .maxArea(intArrayOf(1,8,6,2,5,4,8,3,7))
+        .also { println(it) }
+}
