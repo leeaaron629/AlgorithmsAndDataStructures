76b668d604cdd9308c71ad6544d6faad7b561de6
Aaron Lee <leeaaron326@gmail.com>
2021-11-11 19:39:43 -0500

    Finish implementation of LowestCommonAncestor

---
+++ src/unsolved/LowestCommonAncestor.kt
@@ -0,0 +1,62 @@
+package unsolved
+
+import datastructures.TreeNode
+import java.util.*
+
+/**
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/
+ */
+object LowestCommonAncestor {
+
+    fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
+
+        var node = root
+        val stack = Stack<TreeNode>()
+        val pathP = linkedSetOf<TreeNode>()
+        val pathQ = linkedSetOf<TreeNode>()
+
+        while (true) {
+
+            while (node != null) {
+                stack.push(node)
+                stack.push(node)
+                node = node.left
+            }
+
+            if (stack.empty()) break
+
+            node = stack.pop()
+
+            if (stack.isNotEmpty() && stack.peek() == node) {
+                node = node.right
+            } else {
+                println(node.`val`)
+                if (node == p) {
+                    pathP.addAll(stack)
+                    pathP.add(node)
+                }
+                if (node == q) {
+                    pathQ.addAll(stack)
+                    pathQ.add(node)
+                }
+                node = null
+            }
+
+        }
+
+        println("PathP: ${pathP.joinToString(",") { n -> n.`val`.toString() }}")
+        println("PathQ: ${pathQ.joinToString(",") { n -> n.`val`.toString() }}")
+
+        return pathP.zip(pathQ).lastOrNull { it.first == it.second }?.first
+    }
+
+}
+
+fun main() {
+    val listOne = listOf(1, 2, 3, 4, 5)
+    val listTwo = listOf(1, 2, 3)
+    println("lineOne.zip(listTwo)")
+    listOne.zip(listTwo).forEach { println(it) }
+    println("listTwo.zip(listOne)")
+    listTwo.zip(listOne).forEach { println(it) }
+}
