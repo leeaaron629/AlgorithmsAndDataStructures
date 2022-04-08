a483d275d373171a7caade80ba435897b7d6f2ab
Aaron Lee <leeaaron326@gmail.com>
2022-04-08 00:58:55 -0400

    Solved best_time_to_buy_and_sell.py

---
+++ python/best_time_to_buy_and_sell.py
@@ -0,0 +1,18 @@
+from ast import List
+
+def maxProfit(prices: List[int]) -> int:
+    # [7,1,5,3,6,4,2,10]
+    maxProfit = 0
+    runningMin = prices[0]
+    
+    for p in prices:
+        
+        if p < runningMin:
+            runningMin = p
+            
+        currentProfit = p - runningMin
+        
+        if currentProfit > maxProfit:
+            maxProfit = currentProfit
+            
+    return maxProfit
