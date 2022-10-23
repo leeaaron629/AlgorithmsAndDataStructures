c4bd0ee7d944b898004912dff7fb6b904b7fd60d
Aaron Lee <leeaaron326@gmail.com>
2022-10-23 19:56:15 -0400

    Implemented RedundantConnections with DSU - Unoptimized

--- src/datastructures/DisjointSet.kt
+++ src/datastructures/DisjointSet.kt
@@ -1,7 +1,6 @@
 package datastructures
 
 class DisjointSet<E>(
-    private val disjointSets: MutableMap<E, Set<E>> = mutableMapOf(),
     private val parents: MutableMap<E, E?> = mutableMapOf()
 ) {
 
@@ -24,6 +23,51 @@ class DisjointSet<E>(
         when {
             parent1 == parent2 -> {
             println("Cycle detected, when adding ${listOf(e1, e2)}. Skipping...")
+            }
+            parent1 == e1 && parent2 == e2 -> { // itself is the parent
+                parents[e1] = null
+                parents[e2] = e1
+            }
+            parent1 != e1 && parent2 == e2 -> { // E2 itself is the parent
+                parents[parent2] = parent1
+            }
+            parent1 == e1 && parent2 != e1 -> { // E1 itself is the parent
+                parents[parent1] = parent2
+            }
+            parent1 != e1 && parent2 != e2 -> { // Both E1 and E2 has different parents and belongs to different set
+                parents[parent2] = parent1
+            }
+            else -> error("Illegal state between parents=${listOf(parent1, parent2)} and nodes=${listOf(e1, e2)}")
+        }
+
+    }
+
+}
+
+class DisjointSetDetailed<E>(
+    private val disjointSets: MutableMap<E, Set<E>> = mutableMapOf(),
+    private val parents: MutableMap<E, E?> = mutableMapOf()
+) {
+
+    /**
+     * Given an element E find its set
+     */
+    fun find(e: E): E {
+        var parent: E = e
+        while (parents[parent] != null) {
+            parent = parents[parent] ?: error("parents[parent] cannot be NULL: ${parents[parent]}")
+        }
+        return parent
+    }
+
+    fun union(e1: E, e2: E) {
+
+        val parent1 = find(e1)
+        val parent2 = find(e2)
+
+        when {
+            parent1 == parent2 -> {
+                println("Cycle detected, when adding ${listOf(e1, e2)}. Skipping...")
             }
             parent1 == e1 && parent2 == e2 -> { // itself is the parent
                 disjointSets[e1] = setOf(e1, e2)
---
+++ src/unsolved/RedundantConnections.kt
@@ -0,0 +1,42 @@
+package unsolved
+
+import datastructures.DisjointSet
+import datastructures.DisjointSetDetailed
+
+class RedundantConnections {
+
+    fun findRedundantConnection(edges: Array<IntArray>): IntArray {
+
+        val disjointSet = DisjointSet<Int>()
+
+        for (e in edges) {
+            val p1 = disjointSet.find(e[0])
+            val p2 = disjointSet.find(e[1])
+            if (p1 == p2) return e
+            disjointSet.union(e[0], e[1])
+        }
+
+        return intArrayOf()
+    }
+
+}
+
+fun main() {
+
+    val driver = RedundantConnections()
+    // [[1,2],[1,3],[2,3]]
+    val edges: Array<IntArray> = arrayOf(
+        intArrayOf(1,2), intArrayOf(1,3), intArrayOf(2,3),
+    )
+
+    val disjointSet: DisjointSetDetailed<Int> = DisjointSetDetailed()
+
+    for (e in edges) {
+        println("Union between ${e[0]} & ${e[1]}")
+        disjointSet.union(e1 = e[0], e2 = e[1])
+    }
+
+    println("Disjoint Set:\n$disjointSet")
+
+}
+
