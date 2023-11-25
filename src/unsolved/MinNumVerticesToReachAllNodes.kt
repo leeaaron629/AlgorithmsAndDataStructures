115053dc119e5b24f1277eb57c036da37ed84772
Aaron Lee <aal.malamute@gmail.com>
2023-11-25 13:38:49 -0500

    Added two graph problems for solving

---
+++ src/unsolved/MinNumVerticesToReachAllNodes.kt
@@ -0,0 +1,10 @@
+package unsolved
+
+/**
+ * https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes/
+ */
+class MinNumVerticesToReachAllNodes {
+    fun findSmallestSetOfVertices(n: Int, edges: List<List<Int>>): List<Int> {
+        TODO()
+    }
+}
---
+++ src/unsolved/ShortestPathToVisitAllNodes.kt
@@ -0,0 +1,10 @@
+package unsolved
+
+/**
+ * https://leetcode.com/problems/shortest-path-visiting-all-nodes/description/
+ */
+class ShortestPathToVisitAllNodes {
+    fun shortestPathLength(graph: Array<IntArray>): Int {
+        TODO()
+    }
+}
