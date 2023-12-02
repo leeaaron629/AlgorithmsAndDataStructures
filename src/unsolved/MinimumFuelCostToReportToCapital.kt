966bfb9d88e2d88839936a2e8750030a8169392d
Aaron Lee <leeaaron326@gmail.com>
2023-12-02 11:00:32 -0500

    Added initial problem - https://leetcode.com/problems/minimum-fuel-cost-to-report-to-the-capital/

---
+++ src/unsolved/MinimumFuelCostToReportToCapital.kt
@@ -0,0 +1,12 @@
+package unsolved
+
+/**
+ * https://leetcode.com/problems/minimum-fuel-cost-to-report-to-the-capital/
+ */
+class MinimumFuelCostToReportToCapital {
+
+    fun minimumFuelCost(roads: Array<IntArray>, seats: Int): Long {
+        return 0
+    }
+
+}
