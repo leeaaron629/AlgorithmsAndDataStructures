1b051d41b3078e1d9d4e6c92f547a755ada238a8
Aaron Lee <leeaaron326@gmail.com>
2023-12-04 12:59:06 -0500

    Added NumOfConnectedComponentsInAnUndirectedGraph to be solved with a DisjointedSet

--- src/solved/MinimumFuelCostToReportToCapital.kt
+++ src/solved/MinimumFuelCostToReportToCapital.kt
@@ -1,4 +1,4 @@
-package unsolved
+package solved
 
 import org.junit.jupiter.api.Test
 
---
+++ src/unsolved/NumOfConnectedComponentsInAnUndirectedGraph.kt
@@ -0,0 +1,29 @@
+package unsolved
+
+import org.junit.jupiter.api.Test
+
+/**
+ * https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
+ *
+ * Solve with Disjoint Sets
+ *
+ */
+class NumOfConnectedComponentsInAnUndirectedGraph {
+
+    fun countComponents(n: Int, edges: Array<IntArray>): Int {
+        return 0
+    }
+
+}
+
+class NumberOfConnectedComponentsInAnUndirectedGraphTest {
+
+    private val numOfConnectedComponentsInAnUndirectedGraph = NumOfConnectedComponentsInAnUndirectedGraph()
+
+    @Test
+    fun `test countComponents - Case 1`() {
+
+    }
+
+}
+
