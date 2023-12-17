ab3bb8928954be960af47daea04d7440d9fda115
Aaron Lee <aal.malamute@gmail.com>
2023-12-17 12:05:30 -0500

    Added a test case. Fixed the code, so it's using a boolean insetad of an INT

--- src/unsolved/PacificAtlanticWaterFlow.kt
+++ src/unsolved/PacificAtlanticWaterFlow.kt
@@ -9,8 +9,8 @@ import java.util.*
 class PacificAtlanticWaterFlow {
 
     data class Score(
-        var atlantic: Int = 0,
-        var pacific: Int = 0,
+        var atlantic: Boolean = false,
+        var pacific: Boolean = false,
         var calculated: Boolean = false
     )
 
@@ -47,7 +47,7 @@ class PacificAtlanticWaterFlow {
         val results = mutableListOf<List<Int>>()
         scoreGrid.forEachIndexed { x, rowOfScores ->
             rowOfScores.forEachIndexed { y, score ->
-                if (score.atlantic > 0 && score.pacific > 0) {
+                if (score.atlantic && score.pacific ) {
                     results.add(listOf(x,y))
                 }
             }
@@ -56,7 +56,7 @@ class PacificAtlanticWaterFlow {
         return results
     }
 
-    private fun calcScoresDfs(
+    fun calcScoresDfs(
         x: Int,
         y: Int,
         islandWidth: Int,
@@ -72,8 +72,8 @@ class PacificAtlanticWaterFlow {
 
         val score = Score()
 
-        if (isByThePacific(x, y)) ++score.pacific
-        if (isByTheAtlantic(x, y, islandWidth, islandLength)) ++score.atlantic
+        score.pacific = isByThePacific(x, y)
+        score.atlantic = isByTheAtlantic(x, y, islandWidth, islandLength)
         score.calculated = true
 
         val validLands = getValidNearbyLands(x, y, islandWidth, islandLength)
@@ -82,7 +82,7 @@ class PacificAtlanticWaterFlow {
         val downstreamScores = validLands
             .map { (x, y) -> calcScoresDfs(x, y, islandWidth, islandLength, heights, scoreGrid, visited) }
         val finalScore = downstreamScores
-            .fold(score) { acc, s -> Score(atlantic = acc.atlantic + s.atlantic, pacific = acc.pacific + s.pacific, calculated = true) }
+            .fold(score) { acc, s -> Score(atlantic = acc.atlantic || s.atlantic, pacific = acc.pacific || s.pacific, calculated = true) }
 
         return finalScore
     }
@@ -143,27 +143,38 @@ internal class PacificAtlanticWaterFlowTest {
 
     @Test
     fun `find pacific atlantic squares - case 2`() {
-        // [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
-        // [76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,21],
-        // [75,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,95,22],
-        // [74,143,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,161,96,23],
-        // [73,142,203,256,257,258,259,260,261,262,263,264,265,266,267,268,219,162,97,24],
-        // [72,141,202,255,300,301,302,303,304,305,306,307,308,309,310,269,220,163,98,25],
-        // [71,140,201,254,299,336,337,338,339,340,341,342,343,344,311,270,221,164,99,26],
-        // [70,139,200,253,298,335,364,365,366,367,368,369,370,345,312,271,222,165,100,27],
-        // [69,138,199,252,297,334,363,384,385,386,387,388,371,346,313,272,223,166,101,28],
-        // [68,137,198,251,296,333,362,383,396,397,398,389,372,347,314,273,224,167,102,29],
-        // [67,136,197,250,295,332,361,382,395,400,399,390,373,348,315,274,225,168,103,30],
-        // [66,135,196,249,294,331,360,381,394,393,392,391,374,349,316,275,226,169,104,31],
-        // [65,134,195,248,293,330,359,380,379,378,377,376,375,350,317,276,227,170,105,32],
-        // [64,133,194,247,292,329,358,357,356,355,354,353,352,351,318,277,228,171,106,33],
-        // [63,132,193,246,291,328,327,326,325,324,323,322,321,320,319,278,229,172,107,34],
-        // [62,131,192,245,290,289,288,287,286,285,284,283,282,281,280,279,230,173,108,35],
-        // [61,130,191,244,243,242,241,240,239,238,237,236,235,234,233,232,231,174,109,36],
-        // [60,129,190,189,188,187,186,185,184,183,182,181,180,179,178,177,176,175,110,37],
-        // [59,128,127,126,125,124,123,122,121,120,119,118,117,116,115,114,113,112,111,38],
-        // [58,57,56,55,54,53,52,51,50,49,48,47,46,45,44,43,42,41,40,39]
-
+        val heights = arrayOf(
+            intArrayOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20),
+            intArrayOf(76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,21),
+            intArrayOf(75,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,95,22),
+            intArrayOf(74,143,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,161,96,23),
+            intArrayOf(73,142,203,256,257,258,259,260,261,262,263,264,265,266,267,268,219,162,97,24),
+            intArrayOf(72,141,202,255,300,301,302,303,304,305,306,307,308,309,310,269,220,163,98,25),
+            intArrayOf(71,140,201,254,299,336,337,338,339,340,341,342,343,344,311,270,221,164,99,26),
+            intArrayOf(70,139,200,253,298,335,364,365,366,367,368,369,370,345,312,271,222,165,100,27),
+            intArrayOf(69,138,199,252,297,334,363,384,385,386,387,388,371,346,313,272,223,166,101,28),
+            intArrayOf(68,137,198,251,296,333,362,383,396,397,398,389,372,347,314,273,224,167,102,29),
+            intArrayOf(67,136,197,250,295,332,361,382,395,400,399,390,373,348,315,274,225,168,103,30),
+            intArrayOf(66,135,196,249,294,331,360,381,394,393,392,391,374,349,316,275,226,169,104,31),
+            intArrayOf(65,134,195,248,293,330,359,380,379,378,377,376,375,350,317,276,227,170,105,32),
+            intArrayOf(64,133,194,247,292,329,358,357,356,355,354,353,352,351,318,277,228,171,106,33),
+            intArrayOf(63,132,193,246,291,328,327,326,325,324,323,322,321,320,319,278,229,172,107,34),
+            intArrayOf(62,131,192,245,290,289,288,287,286,285,284,283,282,281,280,279,230,173,108,35),
+            intArrayOf(61,130,191,244,243,242,241,240,239,238,237,236,235,234,233,232,231,174,109,36),
+            intArrayOf(60,129,190,189,188,187,186,185,184,183,182,181,180,179,178,177,176,175,110,37),
+            intArrayOf(59,128,127,126,125,124,123,122,121,120,119,118,117,116,115,114,113,112,111,38),
+            intArrayOf(58,57,56,55,54,53,52,51,50,49,48,47,46,45,44,43,42,41,40,39)
+        )
+        val rowLength = heights[0].size
+        val colLength = heights.size
+        val scoreGrid = Array(colLength) { Array(rowLength) { PacificAtlanticWaterFlow.Score() } }
+//        val result = driver.calcScoresDfs(
+//            x = 10, y = 9, islandWidth = rowLength,
+//            islandLength = colLength, scoreGrid = scoreGrid,
+//            visited = Array(colLength) { BooleanArray(rowLength) { false } }, heights = heights
+//        )
+        val result = driver.pacificAtlantic(heights)
+        println(result)
     }
 
     @Test
