3ac7eb4621e5eddb24cd5313d49272849f58a77f
Aaron Lee <leeaaron326@gmail.com>
2025-02-02 12:44:49 -0500

    Added a comment for union find

--- python/union_find.py
+++ python/union_find.py
@@ -9,5 +9,8 @@ class UnionFind:
             self.parent[x] = self.find(self.parent[x])
         return self.parent[x]
 
+    # union x and y, by setting root_x's parent to root_y
+    # self.parent[root_x] = root_y
+    # now y is the parent of root_x and x
     def union(self, x, y):
-        self.parent[self.find(x)] = self.find(y)
+        self.parent[self.find(x)] = self.find(y) 
