ebe3918ec636aab991df575b31c17a19ed5a841b
Aaron Lee <leeaaron326@gmail.com>
2025-03-04 01:14:12 -0500

    Added MaxNumberOfOccurrencesOfSubstring

---
+++ src/solved/MaxNumberOfOccurrencesOfSubstring.kt
@@ -0,0 +1,23 @@
+package solved
+
+/**
+ * https://leetcode.com/problems/maximum-number-of-occurrences-of-a-substring/
+ */
+object MaxNumberOfOccurrencesOfSubstring {
+    fun maxFreq(s: String, maxLetters: Int, minSize: Int, maxSize: Int): Int {
+        val substringCounts = mutableMapOf<String, Int>().withDefault { 0 }
+        for (i in 0..s.length - minSize) {
+            val substring = s.substring(startIndex = i, endIndex = i + minSize)
+            if (substring.toSet().size <= maxLetters) {
+                substringCounts[substring] = substringCounts.getOrDefault(substring, 0) + 1
+            }
+        }
+        return substringCounts.values.maxOrNull() ?: 0
+    }
+}
+
+fun main() {
+    MaxNumberOfOccurrencesOfSubstring
+        .maxFreq(s = "aababcaababab", maxLetters = 2, minSize = 3, maxSize = 4)
+        .also { assert(it == 3) { "Actual: $it" }  }
+}
